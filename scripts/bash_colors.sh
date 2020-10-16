06994a7097c1c3acd44e7a3fc531896d3bcbb69f
Taks Joseph Refugio <taksjosephrefugio@gmail.com>
2020-10-16 01:14:28 -0500

    Bash Colors Script
    Taks Joseph Refugio [taksjosephrefugio@gmail.com]

---
+++ scripts/bash_colors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LAYOUT_NAME=bash_shell_layout
+LAYOUT_DIR=$(cd .. || exit; pwd)
+LAYOUT="$LAYOUT_DIR/$LAYOUT_NAME"
+BASHRC="$HOME/.bashrc"
+
+if [[ -f $LAYOUT ]]; then
+	echo "$LAYOUT found... "
+	echo "Appending $LAYOUT_NAME to $BASHRC "
+	((echo; cat $LAYOUT) >> $BASHRC) && echo "Appending successful! "
+	echo "NOTE: Restart shell session for changes to take into effect "
+else
+	echo "$LAYOUT_NAME file doesn not exist on $LAYOUT_DIR"
+fi
