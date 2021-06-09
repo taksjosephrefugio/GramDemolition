bbc429322125e192d44a94cdcc62a27945911f02
Taks Joseph Refugio <taksjosephrefugio@gmail.com>
2021-06-08 20:05:00 -0500

    GET_PROJ_DIR_LOCATION
    Signed off by: Taks Joseph Refugio <taksjosephrefugio@gmail.com>

--- scripts/add_env_vars.sh
+++ scripts/add_env_vars.sh
@@ -3,10 +3,31 @@
 # Developer contact: taksjosephrefugio@gmail.com
 #
 
-FAKE_BASHRC="$HOME/Downloads/fake_bashrc"
-REPO_DIR="$(cd ..; pwd || exit)"
+
+####################
+# GLOBAL VARIABLES #
+####################
+FAKE_BASHRC="$HOME/Downloads/nfake_bashrc"
+PROJECTDIR=""
 
 
+# Find out project's base directory location
+get_project_dir_location() {
+    # Keep going up the directory tree until we hit project's base directory
+    while [[ $(basename $(pwd)) != "GramDemolition" ]]; do pushd .. >/dev/null ; done
+
+    # Save project's directory location
+    PROJECTDIR=$(pwd)
+
+    # Return to stack bottom
+    pushd -0 >/dev/null
+
+    # Clear directory stack
+    dirs -c
+}
+
+
+# Parse the contents of the environmental variables file
 read_env_vars() {
     # Go through each line in env_vars
     while read line; do
@@ -14,24 +35,21 @@ read_env_vars() {
         VALUE=${line#*=}
         echo $KEY
         echo $VALUE
-    done < "$REPO_DIR/env_vars"
+    done < "$PROJECTDIR/env_vars"
 }
 
 
 # Find line numbers of previously set Custom Environmental Variables
-find_line_numbers() {
+find_env_vars_section() {
     LINE_NUMBER=0
     while read line; do
         ((LINE_NUMBER+=1))
-        if [[ $line == "#### START - CUSTOM ENVIRONMENTAL VARIABLES" ]]; then
-            START_LINE=$LINE_NUMBER
-        fi
-        if [[ $line == "#### END - CUSTOM ENVIRONMENTAL VARIABLES" ]]; then
-            END_LINE=$LINE_NUMBER
-        fi
+        if [[ $line == "#### START - CUSTOM ENVIRONMENTAL VARIABLES" ]]; then START_LINE=$LINE_NUMBER; fi
+        if [[ $line == "#### END - CUSTOM ENVIRONMENTAL VARIABLES" ]]; then END_LINE=$LINE_NUMBER; fi
     done < $FAKE_BASHRC
 }
 
 
-# Delete previously set Custom Environmental Variables 
-find_line_numbers
+#############
+#  M A I N  #
+#############
