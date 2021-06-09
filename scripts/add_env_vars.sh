108ac71aa60474ef5cfe67bd9c341707048803fc
Taks Joseph Refugio <taksjosephrefugio@gmail.com>
2021-06-09 13:31:04 -0500

    Adding of env vars
    Signed off by: Taks Joseph Refugio <taksjosephrefugio@gmail.com>

--- scripts/add_env_vars.sh
+++ scripts/add_env_vars.sh
@@ -7,8 +7,12 @@
 ####################
 # GLOBAL VARIABLES #
 ####################
-FAKE_BASHRC="$HOME/Downloads/nfake_bashrc"
+BASHRC="$HOME/Downloads/fake_bashrc"
 PROJECTDIR=""
+
+DELIM_START="#### START - CUSTOM ENVIRONMENTAL VARIABLES"
+DELIM_END="#### END - CUSTOM ENVIRONMENTAL VARIABLES"
+
 
 
 # Find out project's base directory location
@@ -28,7 +32,7 @@ get_project_dir_location() {
 
 
 # Parse the contents of the environmental variables file
-read_env_vars() {
+get_env_vars() {
     # Go through each line in env_vars
     while read line; do
         KEY=${line%=*}
@@ -40,16 +44,44 @@ read_env_vars() {
 
 
 # Find line numbers of previously set Custom Environmental Variables
-find_env_vars_section() {
+get_env_vars_section_in_bashrc() {
     LINE_NUMBER=0
     while read line; do
         ((LINE_NUMBER+=1))
-        if [[ $line == "#### START - CUSTOM ENVIRONMENTAL VARIABLES" ]]; then START_LINE=$LINE_NUMBER; fi
-        if [[ $line == "#### END - CUSTOM ENVIRONMENTAL VARIABLES" ]]; then END_LINE=$LINE_NUMBER; fi
-    done < $FAKE_BASHRC
+        if [[ $line == $DELIM_START ]]; then START_LINE=$LINE_NUMBER; fi
+        if [[ $line == $DELIM_END ]]; then END_LINE=$LINE_NUMBER; fi
+    done < $BASHRC
+}
+
+
+add_env_vars() {
+
+    insert_env_vars_section() {
+        echo >> $BASHRC
+        echo $DELIM_START >> $BASHRC
+        cat "$PROJECTDIR/env_vars" >> $BASHRC
+        echo >> $BASHRC
+        echo $DELIM_END >> $BASHRC
+    }
+
+    if [[ ! -z $(tail -c 1 $BASHRC) ]]; then echo >> $BASHRC; fi
+    insert_env_vars_section
 }
 
 
 #############
 #  M A I N  #
 #############
+get_project_dir_location
+get_env_vars_section_in_bashrc
+
+
+if [[ $START_LINE -eq 0 ]]; then
+    # For when .bashrc contains NO existing custom environmental variables
+    add_env_vars
+else
+    # TODO: For when .bashrc contains existing custom environmental variables 
+    echo "Theres something there already"
+    echo $START_LINE
+    echo $END_LINE
+fi
