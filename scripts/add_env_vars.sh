9866f69114a51137d8d3cc194ddbec8cb872523d
Taks Joseph Refugio <taksjosephrefugio@gmail.com>
2021-12-10 21:28:43 -0600

    Created personalized env vars file
    Signed off by: Taks Joseph Refugio <taksjosephrefugio@gmail.com>

--- scripts/add_env_vars.sh
+++ scripts/add_env_vars.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+#
+# Developer contact: taksjosephrefugio@gmail.com
+#
 
-echo "hellothere"
+
+####################
+# GLOBAL VARIABLES #
+####################
+BASHRC="$HOME/Downloads/fake_bashrc"
+PROJECTDIR=""
+
+START_DELIMETER="#### START - CUSTOM ENVIRONMENTAL VARIABLES"
+END_DELIMETER="#### END - CUSTOM ENVIRONMENTAL VARIABLES"
+
+
+# This function finds the starting and ending delimeters for custom environmental variables, if they exist at all. 
+get_env_vars_section_in_bashrc() {
+    local LINE_NUMBER=0
+    local START_LINE=0
+    local END_LINE=0
+    while read line; do
+        ((LINE_NUMBER+=1))
+        if [[ $line == $START_DELIMETER ]]; then START_LINE=$LINE_NUMBER; fi
+        if [[ $line == $END_DELIMETER ]]; then END_LINE=$LINE_NUMBER; fi
+    done < $BASHRC
+    
+    echo $START_LINE $END_LINE
+}
+
