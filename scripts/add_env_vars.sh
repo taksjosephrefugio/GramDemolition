ffe131c588e50e036e9e0d6360cec21bf871d067
Taks Joseph Refugio <taksjosephrefugio@gmail.com>
2021-12-11 12:01:35 -0600

    Polishing add_env_vars.sh
    have a working solution using export and cat
    just need to polish it a bit
    
    Signed off by: Taks Joseph Refugio <taksjosephrefugio@gmail.com>

--- scripts/add_env_vars.sh
+++ scripts/add_env_vars.sh
@@ -9,9 +9,45 @@
 ####################
 BASHRC="$HOME/Downloads/fake_bashrc"
 PROJECTDIR=""
+CUSTOMVARSFILE=$HOME/.custom_env_vars
 
-START_DELIMETER="#### START - CUSTOM ENVIRONMENTAL VARIABLES"
-END_DELIMETER="#### END - CUSTOM ENVIRONMENTAL VARIABLES"
+START_DELIMETER="#### SETTING PERSONALIZED ENVIRONMENTAL VARIABLES"
+END_DELIMETER="#### END OF SETTING PERSONALIZED ENVIRONMENTAL VARIABLES"
+
+
+###################
+# HELPER METHODS  #
+###################
+
+# Find out project's base directory location
+get_project_dir_location() {
+    # Keep going up the directory tree until we hit project's base directory
+    while [[ $(basename $(pwd)) != "GramDemolition" ]]; do pushd .. >/dev/null ; done
+
+    # Save project's directory location
+    retval=$(pwd)
+
+    # Return to stack bottom
+    pushd -0 >/dev/null
+
+    # Clear directory stack
+    dirs -c
+
+    # Echo to return
+    echo $retval
+}
+
+
+# Parse the contents of the environmental variables file
+get_env_vars() {
+    # Go through each line in env_vars
+    while read line; do
+        KEY=${line%=*}
+        VALUE=${line#*=}
+        echo $KEY
+        echo $VALUE
+    done < "$PROJECTDIR/env_vars"
+}
 
 
 # This function finds the starting and ending delimeters for custom environmental variables, if they exist at all. 
@@ -28,3 +64,38 @@ get_env_vars_section_in_bashrc() {
     echo $START_LINE $END_LINE
 }
 
+
+# This function adds an 'export' line on $HOME bashrc so it can read custom environmental variables written on $HOME/.custom_env_vars
+add_env_vars() {
+    local START_LINE=$1
+    local END_LINE=$2
+    local COMMAND=$(echo 'export $(grep -v "^#" {} | xargs -d "\n")' | sed 's|{}|'"$CUSTOMVARSFILE"'|g')
+
+    # Send custom env vars file to $HOME
+    cp $PROJECTDIR/files/custom_env_vars $HOME/.custom_env_vars && echo ">>> Custom environmental variables file sent to $HOME"
+
+    # Insert space if no new endline found on bashrc
+    if [[ ! -z $(tail -c 1 $BASHRC) ]]; then echo >> $BASHRC; fi
+
+    echo >> $BASHRC
+    echo $START_DELIMETER >> $BASHRC
+    echo $COMMAND >> $BASHRC
+    echo $END_DELIMETER >> $BASHRC
+}
+
+
+#############
+#  M A I N  #
+#############
+PROJECTDIR=$(get_project_dir_location)
+
+read START_LINE END_LINE < <(get_env_vars_section_in_bashrc)
+
+if [[ $START_LINE -eq 0 ]]; then
+    # For when .bashrc contains NO existing custom environmental variables
+    add_env_vars && echo ">>> Custom environmental variables have been set successfully!"
+else
+    # For when .bashrc contains existing custom environmental variables
+    echo ">>> Custom environmental variables already being set at Line $START_LINE"
+    exit 1
+fi
